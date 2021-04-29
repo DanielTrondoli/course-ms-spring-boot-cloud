29296ee11339188c3a9fb57f747997ccb27a0e00
Trondoli <daniel.de-paula-trondoli@dxc.com>
2021-04-28 22:12:08 -0300

    Feign Client

--- hr-payroll/src/main/java/com/devdtrondoli/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devdtrondoli/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,11 @@ package com.devdtrondoli.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import jdk.jfr.Enabled;
+
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
