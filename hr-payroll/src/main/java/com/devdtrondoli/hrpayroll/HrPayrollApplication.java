5c96a636f1db51fc89fff121965dbf63b7fa0cff
Trondoli <daniel.de-paula-trondoli@dxc.com>
2021-04-28 22:33:29 -0300

    Ribbon Load Balancing

--- hr-payroll/src/main/java/com/devdtrondoli/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devdtrondoli/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,12 @@ package com.devdtrondoli.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import jdk.jfr.Enabled;
 
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
