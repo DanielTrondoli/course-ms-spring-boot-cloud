062d55d127195116ce38fe4ff0e3bddd0abed8d6
Trondoli <daniel.de-paula-trondoli@dxc.com>
2021-05-02 14:52:08 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/devdtrondoli/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/devdtrondoli/hroauth/entities/User.java
@@ -1,11 +1,17 @@
 package com.devdtrondoli.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
-public class User implements Serializable{	
+public class User implements UserDetails, Serializable{	
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -14,7 +20,7 @@ public class User implements Serializable{
 	private String name;	
 	private String email;
 	private String password;	
-	private Set<Role> Roles = new HashSet<>();
+	private Set<Role> roles = new HashSet<>();
 	
 	
 	public User() {
@@ -62,11 +68,43 @@ public class User implements Serializable{
 	}
 
 	public Set<Role> getRoles() {
-		return Roles;
+		return roles;
 	}
 
 	public void setRoles(Set<Role> roles) {
-		Roles = roles;
+		this.roles = roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+					.map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+					.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 	
