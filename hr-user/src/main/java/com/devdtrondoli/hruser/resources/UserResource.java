9546d4f061ee8f29ca0bcf07379527cfa9a394de
Trondoli <daniel.de-paula-trondoli@dxc.com>
2021-05-02 12:00:37 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/devdtrondoli/hruser/resources/UserResource.java
@@ -0,0 +1,33 @@
+package com.devdtrondoli.hruser.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devdtrondoli.hruser.entities.User;
+import com.devdtrondoli.hruser.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@Autowired
+	private UserRepository reposytory;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = reposytory.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> findById(@RequestParam String email){
+		User obj = reposytory.findByEmail(email);
+		return ResponseEntity.ok(obj);
+	}
+	
+}
